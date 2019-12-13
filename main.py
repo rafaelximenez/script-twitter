faa78ac1135e300ac5cc17f4af06d7ba1ee82610
strapbool <rafael_rx@hotmail.com>
2019-12-13 14:31:58 -0300

    Modificação do main.py com orientação a objetos

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import oauth2
 import json
 import urllib.parse
+from pprint import pprint
 
+keys = ['WRXnyJds71yDayQaXFxPpI2jv', 
+'AjpaogRVFUyuZTfvaNgwj08J0pD3n6f1k08BjUUlapnUFca2w4',
+'799122088594460672-vzJloX2qozKzEuJhNuyB1oew8rJSEzF', 
+'COd1bb65SodeBgD7zPTseuZ9lUBpkeeapROr0MXFprlqz'] 
 
-consumer_key = 'WRXnyJds71yDayQaXFxPpI2jv'
-consumer_secret = 'AjpaogRVFUyuZTfvaNgwj08J0pD3n6f1k08BjUUlapnUFca2w4'
+class twitter:
+    def __init__(self, keys):
+        self.keys = keys
 
-token_key = '799122088594460672-vzJloX2qozKzEuJhNuyB1oew8rJSEzF'
-token_secret = 'COd1bb65SodeBgD7zPTseuZ9lUBpkeeapROr0MXFprlqz'
+    def auth_twitter(self):
+        consumer = oauth2.Consumer(keys[0], keys[1])
+        token = oauth2.Token(keys[2], keys[3])
+        client = oauth2.Client(consumer, token)
+        return client
 
-consumer = oauth2.Consumer(consumer_key, consumer_secret)
-token = oauth2.Token(token_key, token_secret)
-cliente = oauth2.Client(consumer, token)
+    def post_twitter(self, client):
+        query = input("Novo tweet: ")
+        query_coded = urllib.parse.quote(query, safe='')
+        req = client.request('https://api.twitter.com/1.1/statuses/update.json?status=' + query_coded, method='POST')
+        decoded = req[1].decode()
+        obj = json.loads(decoded)
+        return obj
 
-query = input("Novo tweet: ")
-query_codificada = urllib.parse.quote(query, safe='')
-requisicao = cliente.request('https://api.twitter.com/1.1/statuses/update.json?status=' + query_codificada, method='POST')
-
-decodificar = requisicao[1].decode()
-
-objeto = json.loads(decodificar)
-
-print(objeto)
+res = twitter(keys)
+client = res.auth_twitter()
+post = res.post_twitter(client)
+pprint(post)
